9b410eac697eb5f05600f437ab7ff10ac207848c
bac-ta <bac93.it@gmail.com>
2020-08-04 13:22:00 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 #For app
-FROM java:8
-FROM maven:alpine
+FROM maven:3.5.2-jdk-8-alpine AS MAVEN_BUILD
+MAINTAINER bac93.it@gmail.com
+COPY pom.xml /build/
+COPY src /build/src/
+WORKDIR /build/
+RUN mvn package
+FROM openjdk:8-jre-alpine
 WORKDIR /app
-COPY . /app
-RUN mvn -v
-RUN mvn pakage
-LABEL maintainer=bac93.it@gmail.com
-COPY ./target/demo-0.0.1-SNAPSHOT.jar demo-springboot-docker-jenkins.jar
+COPY --from=MAVEN_BUILD /build/target/demo-0.0.1-SNAPSHOT.jar  /app/demo-springboot-docker-jenkins.jar
 EXPOSE 5593
 ENTRYPOINT ["java","-jar","demo-springboot-docker-jenkins.jar"]
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,22 +1,29 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.User;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
-    private UserRepository repository;
+    private final UserRepository repository;
 
     @Autowired
     public UserService(UserRepository repository) {
         this.repository = repository;
     }
 
-    public User findUser(int id) {
-        return repository.findById(id).get();
+    public User findUser(int id) throws ResourceNotFoundException {
+        Optional<User> optionalUser = repository.findById(id);
+        if (!optionalUser.isPresent())
+            throw new ResourceNotFoundException("User not found");
+        return optionalUser.get();
     }
+
     public void deleteUser(int id) {
         repository.deleteById(id);
     }
